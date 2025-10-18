1bf85a8e4c5d03ccd3bb692f856b54e3cca6fac2
Woody <waiyanwoody@gmail.com>
2025-10-18 23:46:59 +0630

    hello world base route

--- src/main/java/com/example/communityforum/api/controller/HelloController.java
+++ src/main/java/com/example/communityforum/api/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.example.communityforum.api.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
-    @GetMapping("/api/hello")
-    public String sayHello() {
-        return "Hello , community forum";
+    @GetMapping("/")
+    public ResponseEntity<Map<String, String>> sayHello() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello world");
+        return ResponseEntity.ok(response);
     }
 }
