97f6240a865a7ea785caefcb8e7d20c3be280910
Woody <waiyanwoody@gmail.com>
2025-11-05 00:16:18 +0630

    try with mock bean

--- src/test/java/com/example/communityforum/CommunityForumApplicationTests.java
+++ src/test/java/com/example/communityforum/CommunityForumApplicationTests.java
@@ -2,9 +2,15 @@ package com.example.communityforum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.example.communityforum.persistence.repository.UserRepository;
 
 @SpringBootTest
 class CommunityForumApplicationTests {
+
+    @MockBean
+    private UserRepository userRepository;
 
     @Test
     void contextLoads() {
